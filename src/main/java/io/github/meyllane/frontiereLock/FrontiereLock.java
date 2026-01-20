04a2943db2f1c518d2c313073e31d67d7c041b76
Meyllane <vilmora.ser@tuta.com>
2026-01-20 19:18:44 +0100

    Base commit

---
+++ src/main/java/io/github/meyllane/frontiereLock/FrontiereLock.java
@@ -0,0 +1,17 @@
+package io.github.meyllane.frontiereLock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FrontiereLock extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
